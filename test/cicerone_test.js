e0a0949eb9d58f66a57d1352aeca10faa1c97c08
goliatone <burgosemi@gmail.com>
2015-11-08 22:25:33 -0500

    test wip, using tape

--- test/cicerone_test.js
+++ test/cicerone_test.js
@@ -1,20 +1,38 @@
 'use strict';
-
-var assert = require('chai').assert;
-var sinon = require('sinon');
+var fs = require('fs');
 var path = require('path');
+var readFile = require('fs').readFileSync;
 var fixture = path.resolve.bind(path, __dirname, 'fixtures');
 
-sinon.assert.expose(assert, { prefix: '' });
-
+var test = require('tape');
 var cicerone = require('..');
 
-
-describe('cicerone', function(){
-
-    describe('constructor', function(){
-        it('should provide a DEFAULTS object', function(){
-            assert.isObject(cicerone.DEFAULTS);
-        });
+test('Cicerone readHosts', function (t) {
+    process.env.CICERONE_PATH = fixture('readHosts/hosts.txt');
+    cicerone.readHosts().then(function(result){
+        t.equal(result, fixtureFile('readHosts/hosts.txt'), 'Should read hosts file');
+        t.end();
     });
 });
+
+test('Cicerone removeHost', function(t){
+    makeHosts('removeHost/hosts.txt');
+    cicerone.removeHost('127.0.0.2', 'cicerone.dev').then(function(hosts){
+        t.equal(hosts, fixtureFile('removeHost/expected.txt'), 'Should read hosts file');
+        t.end();
+    });
+});
+
+function fixtureFile(filepath){
+    return readFile(fixture(filepath), 'utf-8');
+}
+
+function makeHosts(filepath){
+    //copy file
+    filepath = fixture(filepath);
+    var destfile = fixture(filepath) + '.tmp',
+        content = fs.readFileSync(filepath, 'utf-8');
+    fs.writeFileSync(destfile, content, 'utf-8');
+
+    process.env.CICERONE_PATH = destfile;
+}
