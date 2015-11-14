6c7b8618a288183249190b255cc2507158225924
goliatone <burgosemi@gmail.com>
2015-11-13 20:49:59 -0500

    test wip

--- test/cicerone_test.js
+++ test/cicerone_test.js
@@ -5,24 +5,27 @@ var cicerone = require('..');
 
 test('Cicerone removeHost', function(t){
     fixture.make('removeHost/hosts.txt');
+    var expected = fixture.load('removeHost/expected.txt');
     cicerone.removeHost('127.0.0.3', 'cicerone.dev').then(function(hosts){
-        t.equal(hosts, fixture.load('removeHost/expected.txt'), 'Should remove host entry');
+        t.equal(hosts, expected, 'Should remove host entry');
         fixture.clean(t.end);
     });
 });
 
 test('Cicerone readHosts', function (t) {
     fixture.make('readHosts/hosts.txt');
+    var expected = fixture.load('readHosts/hosts.txt');
     cicerone.readHosts().then(function(result){
-        t.equal(result, fixture.load('readHosts/hosts.txt'), 'Should read hosts file');
+        t.equal(result, expected, 'Should read hosts file');
         fixture.clean(t.end);
     });
 });
 
 test('Cicerone addHost', function(t){
     fixture.make('addHost/hosts.txt');
+    var expected = fixture.load('addHost/expected.txt');
     cicerone.addHost('127.0.0.3', 'cicerone.dev').then(function(hosts){
-        t.equal(hosts, fixture.load('addHost/expected.txt'), 'Should add host entry');
+        t.equal(hosts, expected, 'Should add host entry');
         fixture.clean(t.end);
     });
 });
@@ -30,16 +33,18 @@ test('Cicerone addHost', function(t){
 test('Cicerone addHosts', function(t){
     fixture.make('addHosts/hosts.txt');
     var hosts = fixture.load('addHosts/hosts.json');
+    var expected = fixture.load('addHosts/expected.txt');
     cicerone.addHosts(hosts).then(function(hosts){
-        t.equal(hosts, fixture.load('addHosts/expected.txt'), 'Should add an array of host entries');
+        t.equal(hosts, expected, 'Should add an array of host entries');
         fixture.clean(t.end);
     });
 });
 
 test('Cicerone getAllHosts', function (t) {
     fixture.make('getAllHosts/hosts.txt');
+    var expected = fixture.load('getAllHosts/expected.txt');
     cicerone.getAllHosts().then(function(result){
-        t.equal(result, fixture.load('getAllHosts/expected.txt'), 'Should read full hosts file');
+        t.equal(result, expected, 'Should read full hosts file');
         fixture.clean(t.end);
     });
 });
@@ -52,7 +57,7 @@ test('Cicerone hostToObject:domainFirst', function(t){
     t.end();
 });
 
-test('Cicerone hostToObject:ipFirst', function(t){
+test('Cicerone hostToObject:ip', function(t){
     var line = fixture.load('hostToObject/line.txt');
     var result = cicerone.hostToObject(false, line);
     var expected = fixture.load('hostToObject/expected.ip.json');
@@ -60,27 +65,38 @@ test('Cicerone hostToObject:ipFirst', function(t){
     t.end();
 });
 
-test('Cicerone hostsFileJSON', function(t){
-    fixture.make('hostsFileJSON/hosts0.txt');
-    cicerone.hostsFileJSON(true, true).then(function(result){
-        t.deepEqual(result, fixture.load('hostsFileJSON/expected.domain.all.json'), 'Should get domain first all hosts in JSON');
-        fixture.clean(t.end);
-    });
-});
-
-test('Cicerone hostsFileJSON', function(t){
-    fixture.make('hostsFileJSON/hosts1.txt');
-    cicerone.hostsFileJSON(false, true).then(function(result){
-        t.deepEqual(result, fixture.load('hostsFileJSON/expected.ip.all.json'), 'Should get ip first all hosts in JSON');
-        fixture.clean(t.end);
-    });
-});
-
-
-test('Cicerone hostsFileJSON', function(t){
+test('Cicerone hostsFileJSON:domain', function(t){
     fixture.make('hostsFileJSON/hosts3.txt');
+    var expected = fixture.load('hostsFileJSON/expected.domain.json');
     cicerone.hostsFileJSON(true, false).then(function(result){
-        t.deepEqual(result, fixture.load('hostsFileJSON/expected.domain.json'), 'Should get domain first cicerone hosts in JSON');
+        t.deepEqual(result, expected, 'Should get domain first cicerone hosts in JSON');
+        fixture.clean(t.end);
+    });
+});
+
+test('Cicerone hostsFileJSON:domain-all', function(t){
+    fixture.make('hostsFileJSON/hosts0.txt');
+    var expected = fixture.load('hostsFileJSON/expected.domain.all.json');
+    cicerone.hostsFileJSON(true, true).then(function(result){
+        t.deepEqual(result, expected, 'Should get domain first all hosts in JSON');
+        fixture.clean(t.end);
+    });
+});
+
+test('Cicerone hostsFileJSON:ip', function(t){
+    fixture.make('hostsFileJSON/hosts1.txt');
+    var expected = fixture.load('hostsFileJSON/expected.ip.json');
+    cicerone.hostsFileJSON(false, true).then(function(result){
+        t.deepEqual(result, expected, 'Should get ip first hosts in JSON');
+        fixture.clean(t.end);
+    });
+});
+
+test('Cicerone hostsFileJSON:ip-all', function(t){
+    fixture.make('hostsFileJSON/hosts1.txt');
+    var expected = fixture.load('hostsFileJSON/expected.ip.all.json');
+    cicerone.hostsFileJSON(false, true).then(function(result){
+        t.deepEqual(result, expected, 'Should get ip first all hosts in JSON');
         fixture.clean(t.end);
     });
 });
